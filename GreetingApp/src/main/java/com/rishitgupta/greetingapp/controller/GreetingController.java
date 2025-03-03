acf393fe9182419166b14addb0d1441d32bb0576
Rishit Gupta <RishitGupta786@gmail.com>
2025-03-03 14:27:29 +0530

    UC1 Completed -GreetingController

---
+++ GreetingApp/src/main/java/com/rishitgupta/greetingapp/controller/GreetingController.java
@@ -0,0 +1,28 @@
+package com.rishitgupta.greetingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public String getGreeting() {
+        return "{\"message\": \"Hello, World!\"}";
+    }
+
+    @PostMapping
+    public String createGreeting() {
+        return "{\"message\": \"Greeting Created!\"}";
+    }
+
+    @PutMapping
+    public String updateGreeting() {
+        return "{\"message\": \"Greeting Updated!\"}";
+    }
+
+    @DeleteMapping
+    public String deleteGreeting() {
+        return "{\"message\": \"Greeting Deleted!\"}";
+    }
+}
