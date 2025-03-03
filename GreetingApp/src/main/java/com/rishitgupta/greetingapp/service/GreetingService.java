6662389c58f0e6633f7b0ded20756dfec056091d
Rishit Gupta <RishitGupta786@gmail.com>
2025-03-03 14:45:54 +0530

    UC2 Added Service Layer

---
+++ GreetingApp/src/main/java/com/rishitgupta/greetingapp/service/GreetingService.java
@@ -0,0 +1,24 @@
+package com.rishitgupta.greetingapp.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String getGreeting() {
+        return "{\"message\": \"Hello, World!\"}";
+    }
+
+    public String createGreeting() {
+        return "{\"message\": \"Greeting Created!\"}";
+    }
+
+    public String updateGreeting() {
+        return "{\"message\": \"Greeting Updated!\"}";
+    }
+
+    public String deleteGreeting() {
+        return "{\"message\": \"Greeting Deleted!\"}";
+    }
+}
