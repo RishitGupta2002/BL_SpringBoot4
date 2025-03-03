071ac86631a8b565eb764e99de9eaa23112725cf
Rishit Gupta <RishitGupta786@gmail.com>
2025-03-03 11:23:51 +0530

    Greeting app Folder Structure

---
+++ GreetingApp/src/test/java/com/rishitgupta/greetingapp/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishitgupta.greetingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
